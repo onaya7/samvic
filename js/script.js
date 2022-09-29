4b434d3c36d625a94ab740abbbcda70502924325
samuelayano <samuelayano7@gmail.com>
2022-09-29 14:44:16 +0100

    added new styling

--- js/script.js
+++ js/script.js
@@ -26,3 +26,33 @@ const sr = ScrollReveal({
 sr.reveal('.home-text', {delay:280, origin:'bottom'}) 
 
 sr.reveal('.featured, .cta, .new, .brand, .contact', {delay:200, origin:'bottom'})
+
+
+
+
+
+// Quantity wrapper
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+
+let a =1;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    a = (a<10) ? "0" + a : a;
+    num.innerText = a;
+    console.log(a);
+});
+
+minus.addEventListener("click", ()=>{
+    if (a > 1){
+        a--;
+        a = (a<10) ? "0" + a : a;
+        num.innerText = a;
+    }
+});
+
+// Quantity wrapper ends
+
+
